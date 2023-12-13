b4b9ae7f933fc92d8d1575990d6ae4b2c80fcb63
aliumarcdp <aliumar.in@gmail.com>
2023-12-13 15:44:16 +0530

    Add files via upload

---
+++ API_CRUD_Test.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Collections;
+import io.restassured.RestAssured;
+
+public class API_CRUD_Test {
+
+	public static void main(String[] args) {
+		int[] arr = {4,2,1,6,8,71};
+		Arrays.sort(arr);
+		int count=0;
+		int j=0;
+
+		for(int i =0; i<arr.length;i++)
+		{
+				j++;
+				if(j==arr[i])
+				{
+					
+				}
+				else
+				{
+					count++;
+					break;
+				}			
+		}
+		System.out.println("First missing number is:"+ j);
+	}
+}
